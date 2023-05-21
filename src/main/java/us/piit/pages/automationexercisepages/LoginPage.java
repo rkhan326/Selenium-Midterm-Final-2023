804c6be6e7bdbdd2054e27d7bd4a63873749f3b2
erfieee <erfanhaque88@gmail.com>
2023-05-21 00:40:48 -0400

    created pages

--- src/main/java/us/piit/pages/automationexercisepages/LoginPage.java
+++ src/main/java/us/piit/pages/automationexercisepages/LoginPage.java
@@ -1,4 +1,38 @@
 package us.piit.pages.automationexercisepages;
 
-public class LoginPage {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import us.piit.base.CommonAPI;
+
+
+public class LoginPage extends CommonAPI {
+    Logger log = LogManager.getLogger(LoginPage.class.getName());
+    public LoginPage(WebDriver driver){PageFactory.initElements(driver, this);}
+
+    @FindBy(css = "img[alt='Website for automation practice']")
+    WebElement LoginPageHeaderButton;
+
+    @FindBy(css = "input[name='email']")
+    WebElement LoginEmailField;
+
+    @FindBy(css = "input[name='password']")
+    WebElement LoginPasswordField;
+
+    @FindBy(css = "button[data-qa='login-button']")
+    WebElement LoginButton;
+
+    @FindBy(css = "input[name='email']")
+    WebElement SignupEmailField;
+
+    @FindBy(css = "input[name='password']")
+    WebElement SignupPasswordField;
+
+    @FindBy(css = "button[data-qa='signup-button']")
+    WebElement SignupButton;
+
+
 }
---
+++ src/main/java/us/piit/pages/automationexercisepages/VideoTutorialsPage.java
@@ -0,0 +1,8 @@
+package us.piit.pages.automationexercisepages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VideoTutorialsPage {
+    Logger log = LogManager.getLogger(VideoTutorialsPage.class.getName());
+}
