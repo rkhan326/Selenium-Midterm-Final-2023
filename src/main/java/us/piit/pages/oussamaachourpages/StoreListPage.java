e44cf6a32ad7cae0a1e00daea7cc2c564afb0ffd
rkhan326 <rkhan326@gmail.com>
2023-05-20 23:02:42 -0400

    updated commonapi, register test

--- src/main/java/us/piit/pages/oussamaachourpages/StoreListPage.java
+++ src/main/java/us/piit/pages/oussamaachourpages/StoreListPage.java
@@ -1,5 +1,25 @@
 package us.piit.pages.oussamaachourpages;
 
-public class StoreListPage {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import us.piit.base.CommonAPI;
 
+public class StoreListPage extends CommonAPI {
+    Logger log = LogManager.getLogger(StoreListPage.class.getName()) ;
+    public StoreListPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//div[@class='store-data']/h2[1]/a[1]")
+    WebElement storeListNewRegisteredShopName;
+
+    public String getStoreListNewRegisteredShopNameText(){
+        String storeListNewRegisteredShopNameText = getElementText(storeListNewRegisteredShopName);
+        log.info("storelist new registered shopName text retrieval success");
+        return storeListNewRegisteredShopNameText;
+    }
 }
