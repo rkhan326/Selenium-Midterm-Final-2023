487689dbfad1e7ed41e8a3e2c03ab98e9b50adf8
erfieee <erfanhaque88@gmail.com>
2023-05-23 00:56:24 -0400

    signuptest runner added

--- src/main/java/us/piit/pages/automationexercisepages/ProductsPage.java
+++ src/main/java/us/piit/pages/automationexercisepages/ProductsPage.java
@@ -2,8 +2,13 @@ package us.piit.pages.automationexercisepages;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import us.piit.base.CommonAPI;
 
 public class ProductsPage extends CommonAPI {
+
     Logger log = LogManager.getLogger(ProductsPage.class.getName());
+    public ProductsPage(WebDriver driver){PageFactory.initElements(driver, this);}
+
 }
---
+++ src/test/java/automationexercisetest/LogoutTest.java
@@ -0,0 +1,48 @@
+package automationexercisetest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import us.piit.base.CommonAPI;
+import us.piit.pages.automationexercisepages.HomePage;
+import us.piit.pages.automationexercisepages.LoginPage;
+
+public class LogoutTest extends CommonAPI {
+
+    Logger log = LogManager.getLogger(LogoutTest.class.getName());
+
+    @Test
+    public void smoothLogout() {
+
+        LoginPage loginpage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        //validate home page
+        String expectedCategoryHeader = "CATEGORY";
+        String actualCategoryHeader = homePage.getCategoryHeaderText();
+        Assert.assertEquals(expectedCategoryHeader, actualCategoryHeader);
+
+        //click on signup/ login button
+        homePage.clickClickOnSignupLoginButton();
+
+        //validate login page
+        String expectedLoginToYourAccountHeader = "Login to your account";
+        String actualLoginToYourAccountHeader = loginpage.getLoginToYourAccountHeaderText();
+        Assert.assertEquals(expectedLoginToYourAccountHeader, actualLoginToYourAccountHeader);
+
+        //enter login email address, password and click login button
+        loginpage.enterValidLoginEmailInLoginEmailField();
+        loginpage.enterValidLoginPasswordInLoginPasswordField();
+        loginpage.clickClickOnLoginButton();
+
+        //logout
+        loginpage.clickClickOnLogoutButton();
+
+        //validate logout
+        Assert.assertEquals(expectedLoginToYourAccountHeader, actualLoginToYourAccountHeader);
+
+    }
+}
