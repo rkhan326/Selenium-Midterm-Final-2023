eebe7b483632140a5ae68b146ee40ea6f2dabfe9
erfieee <erfanhaque88@gmail.com>
2023-05-31 19:27:55 -0400

    30 test cases

--- src/main/java/us/piit/pages/automationexercisepages/ContactUsPage.java
+++ src/main/java/us/piit/pages/automationexercisepages/ContactUsPage.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.DataProvider;
 import us.piit.base.CommonAPI;
+import us.piit.utility.ExcelReader;
+
+import java.io.File;
 
 
 public class ContactUsPage extends CommonAPI {
@@ -15,6 +18,15 @@ public class ContactUsPage extends CommonAPI {
     Logger log = LogManager.getLogger(ContactUsPage.class.getName());
     public ContactUsPage(WebDriver driver) {PageFactory.initElements(driver, this);}
 
+    String currentDir = System.getProperty("user.dir");
+    String path1 = currentDir + File.separator + "data" + File.separator+"automationExsercise.xlsx";
+
+    ExcelReader excelReader = new ExcelReader(path1);
+
+    String nameDB = excelReader.getStringDataFromCell("automaticExercise",1 ,0);
+    String emailDB = excelReader.getStringDataFromCell("automaticExercise",1 ,1);
+    String subjectDB = excelReader.getStringDataFromCell("automaticExercise",1 ,2);
+    String messageDB = excelReader.getStringDataFromCell("automaticExercise", 1,3);
 
 
     @FindBy(xpath = "//h2[text()='Get In Touch']")
@@ -35,12 +47,14 @@ public class ContactUsPage extends CommonAPI {
         log.info("Ya did it! 'Contact Us' text acquisition success!!");
         return text;
     }
+
+
     public void enterReviewContactUsPage(WebDriver driver) {
-        typeText($contactUsReviewNameField,"Top G");
-        typeText($contactUsReviewEmailField,"topg@gmail.com");
-        typeText($contactUsReviewSubjectField,"Appreciation");
-        typeText($contactUsReviewField,"Thank you very much for this Automation Testing website!");
+        typeText($contactUsReviewNameField,"Joe");
+        typeText($contactUsReviewEmailField,"joe@gmail.com");
+        typeText($contactUsReviewSubjectField,"Cool");
+        typeText($contactUsReviewField,"I'm impressed with the dress!");
         clickWithActions(driver,$submitContactUsReviewButton );
-        log.info("Ya di it! Review submittion success");
+        log.info("Ya did it! Review submittion success");
     }
 }
--- src/test/java/automationexercisetest/ContactUsPageTest.java
+++ src/test/java/automationexercisetest/ContactUsPageTest.java
@@ -2,14 +2,21 @@ package automationexercisetest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import us.piit.base.CommonAPI;
 import us.piit.pages.automationexercisepages.ContactUsPage;
 import us.piit.pages.automationexercisepages.HomePage;
 import us.piit.pages.automationexercisepages.VideoTutorialsPage;
+import us.piit.utility.DataProviderClass;
+import us.piit.utility.ExcelReader;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ContactUsPageTest extends CommonAPI {
     Logger log = LogManager.getLogger(VideoTutorialsPage.class.getName());
+    public static String currentDir = System.getProperty("user.dir");
 
     @Test
     public void contactUsPageButton28() {
@@ -39,4 +46,32 @@ public class ContactUsPageTest extends CommonAPI {
 
         contactUsPage.enterReviewContactUsPage(getDriver()); //submit Review
     }
+
+//    @DataProvider(name = "automaticExercise")
+//    public Object[][] provideAutomaticExerciseReviewData() throws IOException {
+//        String path1 = currentDir + File.separator + "automaticExercise" + File.separator + "automaticExercise.xlsx";
+//        ExcelReader excelReader = new ExcelReader(path1);
+//        Object[][] obj1 = excelReader.getExcelData(path1,"automaticExercise");
+//        return obj1;
+//    }
+//    @Test (dataProvider = "automaticExercise")
+//    public void contactUsSubmitReview30(String nameDB, String emailDB, String subjectDB, String messageDB) {
+//        HomePage homePage = new HomePage(getDriver());
+//        ContactUsPage contactUsPage = new ContactUsPage(getDriver());
+//
+//        homePage.clickOnContactUsButton(); //navigate to products page
+//
+//        //validate page header
+//        String expectedYoutubeHeader = "GET IN TOUCH";
+//        String actualYoutubeHeader= contactUsPage.getContactUsHeaderText();
+//        Assert.assertEquals(actualYoutubeHeader, expectedYoutubeHeader);
+//        log.info(homePage.wellDoneBoy.concat(expectedYoutubeHeader).concat(homePage.headerValidationSuccess));
+//
+//        typeText(contactUsPage.$contactUsReviewNameField,nameDB);
+//        typeText(contactUsPage.$contactUsReviewEmailField,emailDB);
+//        typeText(contactUsPage.$contactUsReviewSubjectField,subjectDB);
+//        typeText(contactUsPage.$contactUsReviewField,messageDB);
+//        clickWithActions(getDriver(),contactUsPage.$submitContactUsReviewButton );
+//        log.info("Ya did it! Review submittion success");
+//    }
 }
